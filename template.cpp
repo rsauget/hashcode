cf10fc1c80cb48cfd86a85359da97b8fa2801b1a
Razvan <stancioiu.razvan@gmail.com>
2016-02-11 14:25:46 +0100

    Add of a template file

---
+++ template.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void read()
+{
+
+}
+
+void write()
+{
+
+}
+
+void compute()
+{
+
+}
+
+void eval()
+{
+
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	read();
+	compute();
+	write();
+	
+	return 0;
+}
